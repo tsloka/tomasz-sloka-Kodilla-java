fa18a84ba8dd97e983ab527642954f92aa7ce777
Suok <suoku667@gmail.com>
2019-06-17 20:53:48 +0200

    first commit

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,33 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+import com.kodilla.testing.user.SimpleUser;
+
 public class TestingMain {
     public static void main(String[] args){
-        System.out.println("Moduł 6. Wprowadzenie do testowania oprogramowania");
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+
+        String result = simpleUser.getUsername();
+
+        System.out.println("First unit test:");
+        if (result.equals("theForumUser")){
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
+
+        System.out.println("Second unit test:");
+
+        Calculator calculator = new Calculator();
+        if (calculator.addAtoB(84, 56) == 140) {
+            System.out.println("Summation ok");
+        } else {
+            System.out.println("Error!");
+        }
+        if (calculator.subtractBfromA(84, 56) == 28) {
+            System.out.println("Subtraction ok");
+        } else {
+            System.out.println("Error!");
+        }
     }
 }
---
+++ kodilla-testing/src/main/java/com/kodilla/testing/calculator/Calculator.java
@@ -0,0 +1,17 @@
+package com.kodilla.testing.calculator;
+
+public class Calculator {
+    int a;
+    int b;
+
+    public Calculator() {
+    }
+
+    public int addAtoB(int a, int b) {
+        return a + b;
+    }
+
+    public int subtractBfromA(int a, int b) {
+        return a - b;
+    }
+}
