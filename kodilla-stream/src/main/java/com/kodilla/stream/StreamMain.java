d4a8c01b406ba29d604c8f66498d206cc9304cbb
Suok <suoku667@gmail.com>
2019-06-25 16:00:06 +0200

    another update

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,6 +1,7 @@
 package com.kodilla.stream;
 
 import com.kodilla.stream.beautifier.PoemBeautifier;
+import com.kodilla.stream.iterate.NumbersGenerator;
 import com.kodilla.stream.lambda.*;
 import com.kodilla.stream.refrence.FunctionalCalculator;
 
@@ -30,5 +31,8 @@ public class StreamMain {
         poemBeautifier.beautify("Challenge Accepted", (toDecorate -> "ABC"+toDecorate+"ABC"));
         poemBeautifier.beautify("Challenge Accepted", (toDecorate -> toDecorate.concat("DOWN")));
         poemBeautifier.beautify("Challenge Accepted", (toDecorate -> toDecorate.substring(2,5)));
+
+        System.out.println("Using Stream to generate even numbers from 1 to 20");
+        NumbersGenerator.generateEven(20);
     }
 }
