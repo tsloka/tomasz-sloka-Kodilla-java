e911c11eae99bf92365046485df0b68ebd863705
Suok <suoku667@gmail.com>
2019-08-07 20:31:28 +0200

    update

--- kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/GoodPatternsMain.java
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/GoodPatternsMain.java
@@ -5,7 +5,9 @@ import com.kodilla.good.patterns.challenges.MovieStore;
 public class GoodPatternsMain {
     public static void main(String[] args) {
         MovieStore movieStore = new MovieStore();
-        movieStore.getMovies().entrySet().stream().forEach(stringListEntry -> System.out.println(stringListEntry.toString()));
-
+        StringBuilder stringBuilder = new StringBuilder();
+        movieStore.getMovies().entrySet().stream().flatMap(entry -> entry.getValue().stream())
+                .forEach(s -> stringBuilder.append(s + " ! "));
+        System.out.println(stringBuilder);
     }
 }
