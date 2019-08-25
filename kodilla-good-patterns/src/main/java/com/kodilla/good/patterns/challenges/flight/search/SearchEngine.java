70db6684d9005dab5ee04480ad7d6f7effe7e150
Suok <suoku667@gmail.com>
2019-08-25 21:26:03 +0200

    food2door update

--- kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/flight/search/SearchEngine.java
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/flight/search/SearchEngine.java
@@ -2,6 +2,7 @@ package com.kodilla.good.patterns.challenges.flight.search;
 
 import java.util.LinkedList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class SearchEngine {
 
@@ -17,21 +18,21 @@ public class SearchEngine {
     }
 
     public void findWithOneTransfer(Cities departure, Cities arrival, FlightDatabase database) {
-        List<Flight> departureFlights = new LinkedList<>();
-        database.getFlights().stream().filter(s -> s.getFrom().equals(departure))
-                .forEach(departureFlights::add);
-        List<Flight> arrivalFlights = new LinkedList<>();
-        database.getFlights().stream().filter(s -> s.getTo().equals(arrival))
-                .forEach(arrivalFlights::add);
+        List<Flight> departureFlights = database.getFlights().stream().filter(s -> s.getFrom().equals(departure))
+                .collect(Collectors.toList());
+        List<Flight> arrivalFlights = database.getFlights().stream().filter(s -> s.getTo().equals(arrival))
+                .collect(Collectors.toList());
         List<SearchResultOneTransfer> results = new LinkedList<>();
 
-        for (Flight flight : departureFlights) {
-            arrivalFlights.stream().filter(s -> s.getFrom().equals(flight.getTo()))
-                    .filter(s->s.getStartingTime().isAfter(flight.getLandingTime()))
-                    .forEach(s->results.add(new SearchResultOneTransfer(flight,s)));
-        }
-        for (SearchResultOneTransfer result : results){
-            System.out.println(result);
-        }
+        departureFlights.stream().forEach(s -> new SearchResultOneTransfer(s,
+                        arrivalFlights.stream().filter(x -> x.getFrom().equals(s.getTo())).
+                                filter(x->x.getStartingTime().isAfter(s.getLandingTime())).findAny().orElse(null)));
+
+//        for (Flight flight : departureFlights) {
+//            arrivalFlights.stream().filter(s -> s.getFrom().equals(flight.getTo()))
+//                    .filter(s -> s.getStartingTime().isAfter(flight.getLandingTime()))
+//                    .forEach(s -> results.add(new SearchResultOneTransfer(flight, s)));
+//        }
+        results.stream().forEach(System.out::println);
     }
 }
