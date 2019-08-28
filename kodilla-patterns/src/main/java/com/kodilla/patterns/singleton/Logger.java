5b674aaa14a0e6d969d25e95a29b86b9bd4ec163
Suok <suoku667@gmail.com>
2019-08-28 11:04:10 +0200

    singlelton update

--- kodilla-patterns/src/main/java/com/kodilla/patterns/singleton/Logger.java
+++ kodilla-patterns/src/main/java/com/kodilla/patterns/singleton/Logger.java
@@ -1,4 +1,29 @@
 package com.kodilla.patterns.singleton;
 
 public class Logger {
+    private static Logger LoggerInstance = null;
+    private String lastLog = "";
+
+    private Logger(){
+    }
+
+    public static Logger getInstance() {
+        if (LoggerInstance == null){
+            synchronized (Logger.class){
+                if (LoggerInstance == null){
+                    LoggerInstance = new Logger();
+                }
+            }
+        }
+        return LoggerInstance;
+    }
+
+    public void log(String log) {
+        lastLog = log;
+        System.out.println("Log: [" + log + "]");
+    }
+
+    public String getLastLog() {
+        return lastLog;
+    }
 }
--- kodilla-patterns/src/test/java/com/kodilla/patterns/singleton/LoggerTestSuite.java
+++ kodilla-patterns/src/test/java/com/kodilla/patterns/singleton/LoggerTestSuite.java
@@ -1,4 +1,25 @@
 package com.kodilla.patterns.singleton;
 
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
 public class LoggerTestSuite {
+    @BeforeClass
+    public static void createLog(){
+        Logger.getInstance().log("Challenge accepted.");
+    }
+
+
+    @Test
+    public void testGetLastLog(){
+        //Given
+        //When
+        String log = Logger.getInstance().getLastLog();
+
+        //Then
+        Assert.assertEquals("Challenge accepted.",log);
+
+    }
 }
