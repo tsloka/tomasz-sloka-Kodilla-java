ddc395e52f9f8007f2c69534085daa35906c5a8b
Suok <suoku667@gmail.com>
2019-09-05 09:45:53 +0200

    Bigmac Builder update

--- kodilla-patterns/src/test/java/com/kodilla/patterns/builder/bigmac/BigmacTestSuite.java
+++ kodilla-patterns/src/test/java/com/kodilla/patterns/builder/bigmac/BigmacTestSuite.java
@@ -20,5 +20,8 @@ public class BigmacTestSuite {
         int numberOfIngredients = bigmac.getIngredients().size();
         //Then
         Assert.assertEquals(4,numberOfIngredients);
+        Assert.assertEquals(Bun.SESAME_BUN,bigmac.getBun());
+        Assert.assertEquals(2,bigmac.getBurgers());
+        Assert.assertNotEquals(Ingredients.ONION,bigmac.getIngredients().equals(Ingredients.ONION));
     }
 }
