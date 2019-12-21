30b1ea4849037bbaeeca910aa8d57374beb2eba0
Suok <suoku667@gmail.com>
2019-12-21 13:48:00 +0100

    Order facade LOGGER update

--- kodilla-patterns2/src/main/java/com/kodilla/kodillapatterns2/aop/calculator/Watcher.java
+++ kodilla-patterns2/src/main/java/com/kodilla/kodillapatterns2/aop/calculator/Watcher.java
@@ -16,6 +16,8 @@ public class Watcher {
     private static final Logger LOGGER = LoggerFactory.getLogger(Watcher.class);
 
     @Before("execution(* com.kodilla.kodillapatterns2.aop.calculator.Calculator.factorial(..))" +
+            "&& args(theNumber) && target(object) || " +
+            "execution(* com.kodilla.kodillapatterns2.facade.api.OrderFacade.processOrder(..)) " +
             "&& args(theNumber) && target(object)")
     public void logEvent(BigDecimal theNumber, Object object) {
         LOGGER.info("Class: " + object.getClass().getName() + ", Args: " + theNumber);
--- kodilla-patterns2/src/test/java/com/kodilla/kodillapatterns2/ShopServiceTestSuite.java
+++ kodilla-patterns2/src/test/java/com/kodilla/kodillapatterns2/ShopServiceTestSuite.java
@@ -5,7 +5,8 @@ import com.kodilla.kodillapatterns2.facade.api.ItemDto;
 import com.kodilla.kodillapatterns2.facade.api.OrderDto;
 import com.kodilla.kodillapatterns2.facade.api.OrderFacade;
 import com.kodilla.kodillapatterns2.facade.api.OrderProcessingException;
-import org.junit.jupiter.api.Test;
+
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
